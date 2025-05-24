7d29cd9b952c3b79ca254df027929d45fc692974
AnilBASARAN <nlbsrn@gmail.com>
2025-05-24 15:56:17 +0300

    corrections

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -36,7 +36,7 @@ onMounted(async ()=>{
         <div class="container-xl lg:container m-auto ">
             <h2 class="text-3xl font-bold text-green-500 mb-6">Browse Jobs</h2>
             <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
- <JobListing v-for="job in state.jobs.slice(0, limit || jobs.length)" :key="job.id" :job="job" />
+ <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
    
             </div>
         </div>
