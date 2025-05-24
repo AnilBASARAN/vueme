1dec582cdc9477ce101d566b584772422fa78fd4
AnilBASARAN <nlbsrn@gmail.com>
2025-05-24 16:03:50 +0300

    added a spinner with a set timeout

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -2,7 +2,7 @@
 import JobListing from './JobListing.vue';
 import axios from 'axios';
 import { ref,defineProps,onMounted, reactive } from 'vue';
-
+import PulseLoader from "vue-spinner/src/PulseLoader.vue"
 defineProps({
     limit:Number,
     showButton: {
@@ -25,6 +25,7 @@ onMounted(async ()=>{
     } catch (error) {
         console.error("Error fetching jobs",error)
     }finally{
+         await new Promise(resolve => setTimeout(resolve, 600));
         state.isLoading = false;
     }
 })
@@ -35,7 +36,12 @@ onMounted(async ()=>{
     <section class="bg-blue-50 px-4 py-10">
         <div class="container-xl lg:container m-auto ">
             <h2 class="text-3xl font-bold text-green-500 mb-6">Browse Jobs</h2>
-            <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            <!-- show loading spinner while loading is true -->
+             <div v-if="state.isLoading" class="text-center text-gray-200 py-6">
+                <PulseLoader />
+             </div>
+             <!-- Show job listing when done loading -->
+            <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
  <JobListing v-for="job in state.jobs.slice(0, limit || state.jobs.length)" :key="job.id" :job="job" />
    
             </div>
